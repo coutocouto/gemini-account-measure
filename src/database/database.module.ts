d0bce54f09ee1d480467a95e1e28f59bf7142e22
Felipe Couto <couto.felipe2@gmail.com>
2024-08-28 20:52:29 -0300

    chore: create a mysql docker compose and add swagger doc

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,17 +1,17 @@
-import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { ConfigService } from '@nestjs/config';
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { ConfigService } from "@nestjs/config";
 
 @Module({
   imports: [
     SequelizeModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
-        dialect: 'mysql',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USER'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
+        dialect: "mysql",
+        host: configService.get("DB_HOST"),
+        port: configService.get("DB_PORT"),
+        username: configService.get("DB_USER"),
+        password: configService.get("DB_PASSWORD"),
+        database: configService.get("DB_NAME"),
         autoLoadModels: true,
         synchronize: true,
       }),
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle("Account Measure API")
+    .setDescription("A API to consult and upload measures")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(80);
 }
 bootstrap();
